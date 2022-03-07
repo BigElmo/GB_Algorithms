657b920a3d0aa80aa948383291fa5d9beee7d99b
BigElmo <bigelmogm@gmail.com>
2022-03-07 19:09:12 +1000

    copy lesson4 source files

---
+++ src/main/java/com/bigelmo/algorithms/lesson3/queue/Queue.java
@@ -0,0 +1,19 @@
+package com.bigelmo.algorithms.lesson3.queue;
+
+public interface Queue<E> {
+
+    boolean insert(E value);
+
+    E remove();
+
+    E peekFront();
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    void display();
+
+}
---
+++ src/main/java/com/bigelmo/algorithms/lesson4/TwoSideLinkedList.java
@@ -0,0 +1,8 @@
+package com.bigelmo.algorithms.lesson4;
+
+public interface TwoSideLinkedList<E>  extends LinkedList<E> {
+
+    void insertLast(E value);
+
+    E getLast();
+}
